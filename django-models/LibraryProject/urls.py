eb69e262356352e017b5de4b8da7cd1c64cbeb6c
Ruffychris <christian.rufus23@gmail.com>
2026-01-25 23:34:23 +0000

    alx django

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,7 +1,20 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    path(
+        'login/',
+        LoginView.as_view(template_name='relationship_app/login.html'),
+        name='login'
+    ),
+    path(
+        'logout/',
+        LogoutView.as_view(template_name='relationship_app/logout.html'),
+        name='logout'
+    ),
+    path('register/', views.register, name='register'),
 ]
