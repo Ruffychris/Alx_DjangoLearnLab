2e66473e95627208e839ca6aab0c44d6588a88dc
Ruffychris <christian.rufus23@gmail.com>
2026-01-25 23:50:44 +0000

    dhjango

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,10 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
 
-
-from django.urls import path
-from .views import add_book, edit_book, delete_book
-
-urlpatterns += [
-    path('add_book/', add_book, name='add_book'),
-    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
-    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),
 ]
+
