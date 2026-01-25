a1574c79ddf03183f92cefc7b89fc3c08dd6a981
Ruffychris <christian.rufus23@gmail.com>
2026-01-25 19:26:46 +0000

    django

--- django-models/LibraryProject/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns to display
-    list_filter = ('author', 'publication_year')            # Filter options
-    search_fields = ('title', 'author')                     # Search box options
-
--- django-models/LibraryProject/bookshelf/delete.md
+++
@@ -1,6 +0,0 @@
-## Delete Book Record
-
-
-from bookshelf.models import Book
-book = Book.objects.get(title="Nineteen Eighty-Four")
-book.delete()
