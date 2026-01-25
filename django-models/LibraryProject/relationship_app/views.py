4f43f74390682c9a4f0aba5e98b4d1524add3c04
Ruffychris <christian.rufus23@gmail.com>
2026-01-25 23:40:24 +0000

    django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,45 +1,30 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Book
-from .models import Library
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 
 
-
-# Function-Based View: List all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
 
 
-# Class-Based View: Detail of a Library
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
 
 
-# Login View
-class UserLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
 
 
-# Logout View
-class UserLogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
 
 
-# Registration View
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
