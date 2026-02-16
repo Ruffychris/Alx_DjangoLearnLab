a33adbae93caf6cfd9852f00a530b96bb38e0cb5
Ruffychris <christian.rufus23@gmail.com>
2026-02-16 16:50:50 +0000

    Fix comment URLs

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -42,21 +42,23 @@ urlpatterns = [
 
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
-        # Comments
-    path(
-        'posts/<int:pk>/comments/new/',
-        CommentCreateView.as_view(),
-        name='comment-create'
-    ),
+    
+    # Comments
+    # Comments
+path(
+    'post/<int:pk>/comments/new/',
+    CommentCreateView.as_view(),
+    name='comment-create'
+),
 
-    path(
-        'comments/<int:pk>/update/',
-        CommentUpdateView.as_view(),
-        name='comment-update'
-    ),
+path(
+    'comment/<int:pk>/update/',
+    CommentUpdateView.as_view(),
+    name='comment-update'
+),
 
-    path(
-        'comments/<int:pk>/delete/',
-        CommentDeleteView.as_view(),
-        name='comment-delete'
-    ),
+path(
+    'comment/<int:pk>/delete/',
+    CommentDeleteView.as_view(),
+    name='comment-delete'
+),
