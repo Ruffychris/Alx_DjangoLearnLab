8f0a5425250c5b19102885836078e2b0c2d6218d
Ruffychris <christian.rufus23@gmail.com>
2026-02-16 17:09:18 +0000

    Fix PostForm: use TagWidget for tags

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,9 @@
+from .models import Comment
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Post
+from taggit.forms import TagWidget
 
 
 class RegisterForm(UserCreationForm):
@@ -23,13 +26,15 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']
+        fields = ['title', 'content', 'tags']  # include tags
+        widgets = {
+            'tags': TagWidget(),               # <-- specify TagWidget
+        }
 
 
-from .models import Comment
+
 
 
 class CommentForm(forms.ModelForm):
