b3e35df189c1f573ecc60129cb107182bf8fac0b
Ruffychris <christian.rufus23@gmail.com>
2026-02-16 18:38:30 +0000

    fix posts/views.py file

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,13 +2,14 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Post
+from accounts.models import User as CustomUser
 from .serializers import PostSerializer
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def feed_view(request):
-    # get the list of users the current user is following
-    following_users = request.user.following.all()  
+    # get the queryset of users the current user follows
+    following_users = CustomUser.objects.filter(followers=request.user)
 
     # autograder expects this exact query
     posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
