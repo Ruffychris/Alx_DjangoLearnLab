954ce9bc60687db604f0e06d0f52d9df9c47a86f
Ruffychris <christian.rufus23@gmail.com>
2026-02-16 18:34:07 +0000

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,7 +4,12 @@ from django.db import models
 class User(AbstractUser):
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',  # now user.following gives users they follow
+        blank=True
+    )
 
     def __str__(self):
         return self.username
