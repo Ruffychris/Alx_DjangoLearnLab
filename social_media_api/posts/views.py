4233ea4c14ceab362eb929f803b06f71e5b6adbe
Ruffychris <christian.rufus23@gmail.com>
2026-02-22 08:52:31 +0000

    new

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -71,10 +71,7 @@ def like_post(request, pk):
 
     post = generics.get_object_or_404(Post, pk=pk)
 
-    like, created = Like.objects.get_or_create(
-        user=request.user,
-        post=post
-    )
+    like, created = Like.objects.get_or_create(user=request.user, post=post)
 
     if not created:
         return Response({'message': 'Already liked'})
